4eed63e67b4ba5ecb9c6cf2ad8c86eb6609d998d
vikash.chandrasekar <core.autocrlf=true>
2021-11-20 22:41:04 +0100

    Airport_Assignment -- included the unit test for DataConfig class and included new test mock files

--- src/test/java/com/acc/airport/config/DataConfigTest.java
+++ src/test/java/com/acc/airport/config/DataConfigTest.java
@@ -1,4 +1,35 @@
 package com.acc.airport.config;
 
+import com.acc.airport.exception.AirportServiceException;
+import com.acc.airport.service.AirportService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.api.easymock.PowerMock;
+
 public class DataConfigTest {
+
+    private AirportService mockAirportService;
+
+    @Before
+    public void setUp() {
+        mockAirportService = PowerMock.createMock(AirportService.class);
+    }
+
+    @Test
+    public void mapDataConfigTest() throws AirportServiceException {
+        get().mapDataValues();
+        Assert.assertNotNull(mockAirportService.countries);
+        Assert.assertNotNull(mockAirportService.airports);
+        Assert.assertNotNull(mockAirportService.runways);
+        Assert.assertEquals("NL", mockAirportService.countries.get(0).getCode());
+        Assert.assertEquals("Netherlands", mockAirportService.countries.get(0).getName());
+        Assert.assertEquals("Amsterdam Airport Schiphol", mockAirportService.airports.get(0).getName());
+        Assert.assertEquals(2513, mockAirportService.runways.get(0).getAirport_ref());
+    }
+
+    private DataConfig get() {
+        return new DataConfig(mockAirportService);
+    }
 }
+
